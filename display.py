8f9276cdd6302faf047e514192fc813e3c7c6792
Ben Schuck <87881945+404Name-Not-Found@users.noreply.github.com>
2025-10-14 20:02:19 -0400

    Should be all good

--- display.py
+++ display.py
@@ -6,51 +6,30 @@ display = Template("""
     <meta charset="UTF-8">
 </head>
 
-<!-- The columns that present the car information in boxes -->
-<div class="columns m-5">
+<body>
+<div class="columns m-5 is-multiline">
+    {% for car in all_cars %}
 
-    <div class="colum p-4">
+<!-- The columns that present the car information in boxes -->
+    <div class="column p-4 is-one-third">
         <div class="box">
-            <figure class"image is-128x128">
+            <figure class="image is-200x200">
                 <img src="/static/images/simplecar.jpg">
             </figure>
             <div class="media-content">
-                <p>
-                    <p class="is-size-3"><strong>Car 1</strong></p>
-                    <br>
+                <p class="is-size-3 mb-1"><strong>{{ car.Model }}</strong></p>
+                <p class="is-size-5 mb-1">
+                    {{ car.Year }} - {{ car.Make }}<br>
+                    {{ car.Color }}<br>
                 </p>
+                <br>
             </div>
         </div>
     </div>
-    
-    <div class="colum p-4">
-        <div class="box">
-            <figure class"image is-128x128">
-                <img src="/static/images/simplecar.jpg">
-            </figure>
-            <div class="media-content">
-                <p>
-                    <p class="is-size-3"><strong>Car 2</strong></p>
-                    <br>
-                </p>
-            </div>
-        </div>
-    </div>
-    
-    <div class="colum p-4">
-        <div class="box">
-             <figure class"image is-128x128">
-                <img src="/static/images/simplecar.jpg">
-            </figure>
-            <div class="media-content">
-                <p>
-                    <p class="is-size-3"><strong>Car 3</strong></p>
-                    <br>
-                </p>
-            </div>
-        </div>
-    </div>
+
+    {% endfor %}
 </div>
+</body>
 
 <html>
 """)
